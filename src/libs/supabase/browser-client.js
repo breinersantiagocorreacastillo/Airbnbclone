127773be2120767d1c83bb880d677169c87d5896
breinersantiagocorreacastillo <39597357+breinersantiagocorreacastillo@users.noreply.github.com>
2025-12-02 14:11:33 -0500

    Refactor supabaseBrowser to be a function

--- src/libs/supabase/browser-client.js
+++ src/libs/supabase/browser-client.js
@@ -1,5 +1,10 @@
-import { createBrowserClient } from '@supabase/ssr'; 
-export const supabaseBrowser = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+'use client';
+
+import { createBrowserClient } from '@supabase/ssr';
+
+export function supabaseBrowser() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+}
